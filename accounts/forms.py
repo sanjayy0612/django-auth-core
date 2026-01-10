4348c1488f370167f0020f2dc81828c29f49cbca
sanjayy0612 <sanjayelango06@gmail.com>
2026-01-10 16:44:37 +0530

    removed pass

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,23 +1,21 @@
-from django.contrib.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django import forms
-from django.contrib.auth.Models import User
 
 
 class SignUpForm(UserCreationForm):
 
     #Name
-    First_Name=forms.CharField(, max_length=15, required=True)
-    Last_Name=forms.forms.CharField(, max_length=10, required=True)
+    First_Name=forms.CharField( max_length=15, required=True)
+    Last_Name=forms.CharField(, max_length=10, required=True)
 
     #Gmail
     Mail=forms.forms.EmailField(, required=True)
 
-    #password
-
-    Password=forms.forms.CharField(, max_length=20, required=True)
+    
 
     class meta():
         model=User
-        fields=("First_Name","Last_Name","Mail","Password")
+        fields=("First_Name","Last_Name","Mail")
 
 
