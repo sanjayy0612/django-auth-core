809aae6306b51281ee1d2240e8180fd626dc2b81
sanjayy0612 <sanjayelango06@gmail.com>
2026-01-11 21:46:43 +0530

    workiing

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,20 +4,22 @@ from django import forms
 
 
 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
 class SignUpForm(UserCreationForm):
-
-    #Name
-    First_Name=forms.CharField( max_length=15, required=True)
-    Last_Name=forms.CharField( max_length=10, required=True)
-
-    #Gmail
-    Mail=forms.EmailField(required=True)
-
     
+    First_Name = forms.CharField(max_length=15, required=True)
+    Last_Name = forms.CharField(max_length=10, required=True)
+    Mail = forms.EmailField(required=True)
+    
+    
+    username = forms.CharField(max_length=150, required=True, help_text="Required. 150 characters or fewer.")
 
-    class Meta():
-        model=User
-        fields=("First_Name","First_Name","Last_Name","Mail")
+    class Meta:
+        model = User
+        fields = ("username", "First_Name", "Last_Name", "Mail", "password1", "password2")
 
 
 class Login_Form(AuthenticationForm):
--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout
-from .forms import SignUpForm,login_form
+from .forms import SignUpForm,Login_Form
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 # Create your views here.
 def signup_view(request):
-    
-    if request.method=='POST':
-        form=SignUpForm(request.POST)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
         if form.is_valid():
-            user=form.save()
-            login(request,user)
-            return redirect("homepage")
+            user = form.save()
+            messages.success(request, f'Welcome {user.username}! Account created successfully!')
+            return redirect('/accounts/')  # or 'home'
     else:
-        form=SignUpForm()
-    
-    return render(request,'accounts/signup.html',{"form":form})
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {"form": form})
 
 def if_call_accounts(request):
     return redirect('signup')
@@ -25,19 +25,22 @@ def Home_view(request):
 
 def login_view(request):
     if request.method == 'POST':
-        form = login_form(data=request.POST) 
+        form = Login_Form(data=request.POST) 
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('homepage') 
+            messages.success(request,"Logged In ")
     else:
-        form = login_form()
+        form = Login_Form()
 
     return render(request, 'accounts/login.html', {'form': form})
-    
+
 def logout_view(request):
     logout(request)
+    messages.info(request,"logged Out")
     return redirect('login')
+    
 
 
 @login_required(login_url='login')
